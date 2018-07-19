b0b93fcc1710ec5c2bc470f3489f271fc699c93f
Fred <34234759+FredsFactory@users.noreply.github.com>
2018-07-19 20:51:14 +0200

    Add files via upload

--- generateWingRibs.py
+++ generateWingRibs.py
@@ -30,7 +30,7 @@ profil_construction_aile=[]
 panel=[]
 wing_right=[]
 couple=[]
-distanceinternervure=[-13.07,13.07+44.18,42.86,43.66,59.67,58.07,57.90,58.14,57.39,57.84,58.37,57.47,57.92,57.95,57.84,57.38,58.04,58.28,57.99,57.84,54.80,65.02,58.41,57.80,58.33,57.88,58.59,58.78,56.98,63.74,52.07,52.01,46.30 ]
+distanceinternervure=[-7.07,13.07+44.18,42.86,43.66,59.67,58.07,57.90,58.14,57.39,57.84,58.37,57.47,57.92,57.95,57.84,57.38,58.04,58.28,57.99,57.84,54.80,65.02,58.41,57.80,58.33,57.88,58.59,58.78,56.98,63.74,52.07,52.01,46.30 ] #premiere nervure -13.07
 anglenervureX=[85.5,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90]
 
 
@@ -38,31 +38,70 @@ anglenervureX=[85.5,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,90,
 # Construction des plans de decoupe
 #------------------------------------------------------------------
 def generateWingRibs(name):
+ b=Draft.clone(FreeCAD.ActiveDocument.wing_r)
  pos=distanceinternervure[0]
- for i in range(0,32,1):
+ for i in range(0,25,1):
     p1=FreeCAD.Placement()
     p1.Rotation.Q=(0.0,1.0,0.0,0.0)
     p1.Base=FreeCAD.Vector(-300,-100,0.0)
     Plan_coupe = Draft.makeRectangle(length=700.0,height=200.0,placement=p1,face=True,support=None)
-    Plan_coupe.Placement=FreeCAD.Placement(FreeCAD.Vector(-200,pos,-100),FreeCAD.Rotation(FreeCAD.Vector(1,0,0),anglenervureX[i]))
+    Plan_coupe.Placement=FreeCAD.Placement(FreeCAD.Vector(-200,long(pos),-100),FreeCAD.Rotation(FreeCAD.Vector(1,0,0),anglenervureX[i]))
     FreeCAD.ActiveDocument.recompute()
     col=[(1.5,0.5,0.5)]
     FreeCAD.ActiveDocument.Rectangle.ViewObject.DiffuseColor=col
     FreeCAD.ActiveDocument.Rectangle.ViewObject.Transparency=90
-    Plan_coupe.Label="PlanDeCoupe_"+str(pos)+"mm"
-    #Draft.clone(FreeCAD.ActiveDocument.wing_r)
+    Plan_coupe.Label="PlanDeCoupe_"+str(long(pos))+"mm"
+    
+    f = FreeCAD.activeDocument().addObject('Part::Extrusion', 'Extrude')
+    #f = App.getDocument('AirPlane').getObject('Extrude')
+    f.Base = Plan_coupe#App.getDocument('AirPlane').getObject('Nerv_021')
+    f.DirMode = "Normal"
+    f.DirLink = None
+    f.LengthFwd = 2.000000000000000
+    f.LengthRev = 0.000000000000000
+    f.Solid = True
+    f.Reversed = False
+    f.Symmetric = False
+    f.TaperAngle = 0.000000000000000
+    f.TaperAngleRev = 0.000000000000000
+        #Gui.ActiveDocument.Extrude.ShapeColor=Gui.ActiveDocument.Nerv_021.ShapeColor
+        #Gui.ActiveDocument.Extrude.LineColor=Gui.ActiveDocument.Nerv_021.LineColor
+        #Gui.ActiveDocument.Extrude.PointColor=Gui.ActiveDocument.Nerv_021.PointColor
+    f.Base.ViewObject.hide()
+    FreeCAD.ActiveDocument.recompute()
+    
+    
+    
+    
+    
+    
+    
+    a=FreeCAD.activeDocument().addObject("Part::MultiCommon","Nerv_")#"Common")
+    #a.Shapes = [FreeCAD.activeDocument().Rectangle,b,]#FreeCAD.activeDocument().Clone,]
+    a.Shapes = [b,f,]#Plan_coupe,]
+    a.Label="Nerv_"+str(pos)+"mm"
+    
+    
     pos=pos+distanceinternervure[i+1]
     print pos
     print i
- a=FreeCAD.activeDocument().addObject("Part::MultiCommon","Common")
- a.Shapes = [App.activeDocument().Rectangle,App.activeDocument().Clone,]
- a.Label="Nerv"+str(i)
- Gui.activeDocument().Rectangle.Visibility=False
- Gui.activeDocument().Clone.Visibility=False
- Gui.ActiveDocument.Common.ShapeColor=Gui.ActiveDocument.Rectangle.ShapeColor
- Gui.ActiveDocument.Common.DisplayMode=Gui.ActiveDocument.Rectangle.DisplayMode
- App.ActiveDocument.recompute()
- couple.Append(a)
+   
+    
+    
+    #a.Label="Nerv"+str(i)
+
+
+
+
+
+
+#FreeCAD.ActiveDocument().Rectangle.ViewObject.Visibility=False
+#   FreeCAD.ActiveDocument().Clone.Visibility=False
+#   FreeCAD.ActiveDocument.Common.ShapeColor=Gui.ActiveDocument.Rectangle.ShapeColor
+#   FreeCAD.ActiveDocument.Common.DisplayMode=Gui.ActiveDocument.Rectangle.DisplayMode
+
+    couple.append(a)
+    FreeCAD.ActiveDocument.recompute()
  return
 
 class GenerateWingRibsCommand():
