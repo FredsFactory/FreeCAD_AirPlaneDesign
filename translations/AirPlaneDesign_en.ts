fb0054f16f0da7a16d46e22a0451427435d99d61
Fred <34234759+FredsFactory@users.noreply.github.com>
2019-12-24 14:33:56 +0100

    translations

---
+++ translations/AirPlaneDesign_en.ts
@@ -0,0 +1,248 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="14"/>
+        <location filename="../resources/selectRibProfil.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="33"/>
+        <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="55"/>
+        <source>Fuselage Length</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="78"/>
+        <source>Fuselage Width</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="91"/>
+        <source>Fuselage Height</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="124"/>
+        <source>Nose Lever</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="137"/>
+        <source>Wing Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="160"/>
+        <location filename="../resources/selectRibProfil.ui" line="222"/>
+        <source>Shape</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="173"/>
+        <source>Girder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="256"/>
+        <source>Wing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="268"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Débuter par la saisie du nombre de panneaux&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="284"/>
+        <source>1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="289"/>
+        <source>Panel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="294"/>
+        <source>Profil</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="299"/>
+        <source>File (dat)</source>
+        <extracomment>File (dat)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="304"/>
+        <source>Corde emplature</source>
+        <extracomment>Root Chord </extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="309"/>
+        <source>Corde saumon</source>
+        <extracomment>Tip chord</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="314"/>
+        <source>Longueur paneau</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="319"/>
+        <source>X emplature</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="324"/>
+        <source>X saumon</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="329"/>
+        <source>Z emplature</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="334"/>
+        <source>Z saumon</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="339"/>
+        <source>Rotation X</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="344"/>
+        <source>Rotation Y</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="349"/>
+        <source>RotationZ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="354"/>
+        <source>Nombre de nervure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="380"/>
+        <source>Nombre de paneaux/Number of panels:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="402"/>
+        <source>Fill in Sheet with an example</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="408"/>
+        <source>Rudder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="413"/>
+        <source>Elevator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/airPlaneDesignWingdialog.ui" line="450"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/selectRibProfil.ui" line="46"/>
+        <source>Import DAT File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/selectRibProfil.ui" line="58"/>
+        <source>Select the profil</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/selectRibProfil.ui" line="74"/>
+        <source>NACA Generator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/selectRibProfil.ui" line="86"/>
+        <source>NACA</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/selectRibProfil.ui" line="99"/>
+        <source>Number of Points</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/selectRibProfil.ui" line="168"/>
+        <location filename="../resources/selectRibProfil.ui" line="251"/>
+        <source>mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/selectRibProfil.ui" line="187"/>
+        <source>Chord in mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/selectRibProfil.ui" line="200"/>
+        <location filename="../resources/selectRibProfil.ui" line="219"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;If select the rib will be created with BSpline&lt;/p&gt;&lt;p&gt;If not  the rib will be created with with wire&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/selectRibProfil.ui" line="203"/>
+        <source>Make a BSpline</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../resources/selectRibProfil.ui" line="238"/>
+        <source>Thickness in mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Air Plane Design</name>
+    <message>
+        <location filename="../InitGui.py" line="69"/>
+        <source>Air Plane &amp;Design</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AirPlaneDesign</name>
+    <message>
+        <location filename="../InitGui.py" line="63"/>
+        <source>Air Plane Design</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>App::Property</name>
+    <message>
+        <location filename="../airPlaneRib.py" line="51"/>
+        <source>Profil type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../airPlaneRib.py" line="53"/>
+        <source>NacaProfil</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
