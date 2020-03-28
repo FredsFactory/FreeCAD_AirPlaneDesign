0a6005951ddd052fc06bf6dbe3b835a8167731cf
Fred <fniv@free.fr>
2020-03-28 22:36:12 +0100

    RibTaskPanel V0.0
    Creation of a rib editing panel. A double click on the rib object opens the panel.  
    RibTaskPanel V0.0
    
    Creation of a rib editing panel. A double click on the rib object opens the panel.  
    pycache delete

--- airPlaneRib.py
+++ airPlaneRib.py
@@ -27,6 +27,7 @@ __url__ = "https://fredsfactory.fr"
 import FreeCAD,FreeCADGui
 
 from PySide import QtCore
+from PySide import QtGui
 from airPlaneAirFoil import process,decodeName
 from airPlaneDesingProfilUI import SelectObjectUI
 from airPlaneAirFoilNaca import generateNaca
@@ -51,7 +52,6 @@ class WingRib:
         obj.addProperty("App::PropertyInteger","NacaNbrPoint","NacaProfil",QtCore.QT_TRANSLATE_NOOP("App::Property","Naca Number of Points")).NacaNbrPoint=_nacaNbrPoint
         obj.addProperty("App::PropertyBool","finite_TE","NacaProfil",QtCore.QT_TRANSLATE_NOOP("App::Property","Use a finite thickness at TE")).finite_TE=_finite_TE
         obj.addProperty("App::PropertyBool","useSpline","Rib",QtCore.QT_TRANSLATE_NOOP("App::Property","use Spline")).useSpline =_useSpline
-        #obj.addProperty("App::PropertyLength","Chord","Rib","chord").Chord=_chord
         obj.addProperty("App::PropertyLength","Chord","Rib",QtCore.QT_TRANSLATE_NOOP("App::Property","Chord")).Chord=_chord
         obj.addProperty("App::PropertyLength","Thickness","Rib",QtCore.QT_TRANSLATE_NOOP("App::Property","Thickness")).Thickness=_thickness
         obj.addProperty("App::PropertyLength","xrot","Rib","chord").xrot=_xrot
@@ -93,6 +93,109 @@ class WingRib:
             fp.Shape = fp.Shape.extrude(FreeCAD.Base.Vector(0,fp.Thickness,0))
 
         FreeCAD.Console.PrintMessage("Create Rib End\n")
+
+
+class RibTaskPanel: 
+    '''A TaskPanel for the Rib'''
+    def __init__(self,vobj): 
+        print("init RibTaskPanel")
+        self.obj = vobj
+        path_to_ui = FreeCAD.getUserAppDataDir()+ 'Mod/AirPlaneDesign/resources/ribTaskPanel.ui'  
+        self.form = FreeCADGui.PySideUic.loadUi(path_to_ui)
+        self.update(vobj)
+
+    def isAllowedAlterSelection(self):
+        return True
+
+    def isAllowedAlterView(self):
+        return True
+
+    def getStandardButtons(self):
+        return int(QtGui.QDialogButtonBox.Ok)
+
+    def updateGraphicsViewRib(self):
+        #coords=_coords 
+        fp=self.obj.Object   
+        coords=fp.Coordinates
+                
+        for row_number,row_data in enumerate(coords):
+            self.form.profilTable.insertRow(row_number)
+            for col_number, data in enumerate(row_data):
+               self.form.profilTable.setItem(row_number,col_number,QtGui.QTableWidgetItem(str(data)))
+        
+        scale=self.form.chord.value()*2
+        scene=QtGui.QGraphicsScene()
+        self.form.ribView.setScene(scene)
+            
+        points=[]
+        first_v = None
+        last_v = None
+        for v in coords:
+                 if first_v is None:
+                     first_v = v
+            # End of if first_v is None
+            # Line between v and last_v if they're not equal
+                 if (last_v != None) and (last_v != v):
+                     points.append(QtCore.QPointF(last_v.x*scale,-last_v.z*scale ))
+                     points.append(QtCore.QPointF(v.x*scale,-v.z*scale ))
+            # End of if (last_v != None) and (last_v != v)
+            # The new last_v
+                 last_v = v
+
+        # End of for v in upper
+        # close the wire if needed
+        if last_v != first_v:
+                     points.append(QtCore.QPointF(last_v.x*scale,-last_v.z*scale ))
+                     points.append(QtCore.QPointF(first_v.x*scale,-first_v.z*scale ))
+        item=QtGui.QGraphicsPolygonItem(QtGui.QPolygonF(points))
+        item.setPen(QtGui.QPen(QtCore.Qt.blue))
+        scene.addItem(item)
+        self.form.ribView.setFocus()
+        self.form.ribView.show()
+
+        return
+
+
+    def update(self,vobj):
+        'fills the dialog with rib properties'
+        
+        self.form.thickness.setValue(vobj.Object.Thickness)
+        self.form.chord.setValue(vobj.Object.Chord)
+        self.form.kingOfLines.setChecked(vobj.Object.useSpline)
+        
+        self.form.fileName.setText(vobj.Object.RibProfil)
+        
+        self.form.NACANumber.setText(vobj.Object.NacaProfil)
+        self.form.nacaNbrPoint.setValue(vobj.Object.NacaNbrPoint)
+        self.form.finite_TE.setChecked(vobj.Object.finite_TE)
+        
+        
+        for row_number,row_data in enumerate(vobj.Object.Coordinates):
+            self.form.profilTable.insertRow(row_number)
+            for col_number, data in enumerate(row_data):
+               self.form.profilTable.setItem(row_number,col_number,QtGui.QTableWidgetItem(str(data)))
+        self.updateGraphicsViewRib()
+    
+    def accept(self):
+        '''Update properties of Rib'''
+        print("accept")
+        fp=self.obj.Object
+        fp.Thickness=self.form.thickness.value()
+        fp.Chord=self.form.chord.value()
+        fp.useSpline=self.form.kingOfLines.isChecked()
+        
+        
+        
+        
+        FreeCAD.ActiveDocument.recompute()
+        FreeCADGui.ActiveDocument.resetEdit()
+        return True
+
+    def retranslateUi(self, TaskPanel):
+        #TaskPanel.setWindowTitle(QtGui.QApplication.translate("draft", "Faces", None))
+        #self.addButton.setText(QtGui.QApplication.translate("draft", "Update", None))
+        print("")
+
 
 class ViewProviderWingRib:
     def __init__(self, obj):
@@ -145,6 +248,17 @@ class ViewProviderWingRib:
         '''When restoring the serialized object from document we have the chance to set some internals here.\
             Since no data were serialized nothing needs to be done here.'''
         return None
+
+    def setEdit(self,vobj,mode):
+        #print("setEdit start")
+        taskd = RibTaskPanel(vobj)
+        #taskd.obj = vobj.Object
+        #taskd.update()
+        #self.Object.ViewObject.Visibility=False
+        #self.Object.baseObject[0].ViewObject.Visibility=True
+        FreeCADGui.Control.showDialog(taskd)
+        return True
+
 
 class CommandWingRib:
     "the WingPanel command definition"
