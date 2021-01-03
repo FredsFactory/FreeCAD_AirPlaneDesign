db035f388591f03262be5affbf56a98f3bbd7817
Fred <fniv@free.fr>
2021-01-03 20:22:10 +0100

    V0.41
    crash when creating a wing

--- airPlanePlane.py
+++ airPlanePlane.py
@@ -1,21 +1,21 @@
 #################################################
 #
 # Airfoil creation - Aircraft
-# 
+#
 # Copyright (c) F. Nivoix - 2019 - V0.4
 #
 # For FreeCAD Versions = or > 0.17 Revision xxxx
 #
-# This program is free software; you can redistribute it and/or modify  
-# it under the terms of the GNU Lesser General Public License (LGPL)    
-# as published by the Free Software Foundation; either version 2 of     
-# the License, or (at your option) any later version.                   
-# for detail see the LICENCE text file.                                 
-#                                                                         
-# This program is distributed in the hope that it will be useful,       
-# but WITHOUT ANY WARRANTY; without even the implied warranty of        
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         
-# GNU Library General Public License for more details. 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (LGPL)
+# as published by the Free Software Foundation; either version 2 of
+# the License, or (at your option) any later version.
+# for detail see the LICENCE text file.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
 #
 ################################################
 
@@ -45,12 +45,12 @@ class Plane:
         obj.addProperty("App::PropertyLength", "fuselageLength", "fuselage", QtCore.QT_TRANSLATE_NOOP("App::Property", "Fuselage Length"))
         obj.addProperty("App::PropertyLength", "fuselageHeigth", "fuselage", QtCore.QT_TRANSLATE_NOOP("App::Property", "Fuselage Heigth"))
         obj.addProperty("App::PropertyLength", "fuselageWidth", "fuselage", QtCore.QT_TRANSLATE_NOOP("App::Property", "Fuselage Width"))
-        
-        
+
+
         obj.addProperty("App::PropertyFile", "_3viewsFile", "fuselage", QtCore.QT_TRANSLATE_NOOP("App::Property", "3 Views file"))
-                 
+
         obj.addProperty("App::PropertyLinkList", "Wings", "Base", QtCore.QT_TRANSLATE_NOOP("App::Property", "List of wings"))
-        
+
 
     def onChanged(self, fp, prop):
         '''Do something when a property has changed'''
@@ -77,12 +77,12 @@ class Plane:
             op.Path.Center = self.obj.Operations.Path.Center
 
 
-class PlaneTaskPanel: 
+class PlaneTaskPanel:
     '''A TaskPanel for the Rib'''
-    def __init__(self,vobj): 
+    def __init__(self,vobj):
         print("init RibTaskPanel")
         self.obj = vobj
-        path_to_ui = FreeCAD.getUserAppDataDir()+ 'Mod/AirPlaneDesign/resources/airPlaneDesignEdit.ui'  
+        path_to_ui = FreeCAD.getUserAppDataDir()+ 'Mod/AirPlaneDesign/resources/airPlaneDesignEdit.ui'
         self.form = FreeCADGui.PySideUic.loadUi(path_to_ui)
         self.update(vobj)
 
@@ -101,12 +101,12 @@ class PlaneTaskPanel:
         print('update')
         self.form.fuselageLength.setValue(vobj.Object.fuselageLength)
         self.form.fuselageHeigth.setValue(vobj.Object.fuselageHeigth)
-        self.form.fuselageWidth.setValue(vobj.Object.fuselageWidth) 
-        #self.form.fileName.setText(vobj.Object.RibProfil) 
+        self.form.fuselageWidth.setValue(vobj.Object.fuselageWidth)
+        #self.form.fileName.setText(vobj.Object.RibProfil)
         #self.form.NACANumber.setText(vobj.Object.NacaProfil)
         #self.form.nacaNbrPoint.setValue(vobj.Object.NacaNbrPoint)
         #self.form.finite_TE.setChecked(vobj.Object.finite_TE)
-    
+
     def accept(self):
         '''Update properties of Rib'''
         print("accept")
@@ -114,7 +114,7 @@ class PlaneTaskPanel:
         fp.fuselageLength=self.form.fuselageLength.value()
         fp.fuselageHeigth=self.form.fuselageHeigth.value()
         fp.fuselageWidth=self.form.fuselageWidth.value()
-        
+
         FreeCAD.ActiveDocument.recompute()
         FreeCADGui.ActiveDocument.resetEdit()
         return True
@@ -130,27 +130,22 @@ class ViewProviderPlane:
         '''Set this object to the proxy object of the actual view provider'''
         obj.addProperty("App::PropertyColor","Color","Wing","Color of the wing").Color=(1.0,0.0,0.0)
         obj.Proxy = self
-    
+
     def getDefaultDisplayMode(self):
         '''Return the name of the default display mode. It must be defined in getDisplayModes.'''
         return "Flat Lines"
-    
-    def getIconold(self):
-        '''Return the icon in XPM format which will appear in the tree view. This method is\
-            optional and if not defined a default icon is shown.'''
-        return os.path.join(smWB_icons_path,'plane.xpm')
-    
+
     def getIcon(self):
         '''Return the icon in XPM format which will appear in the tree view. This method is\
             optional and if not defined a default icon is shown.'''
         return os.path.join(smWB_icons_path,'plane.xpm')
-    
+
     def __getstate__(self):
         '''When saving the document this object gets stored using Python's json module.\
             Since we have some un-serializable parts here -- the Coin stuff -- we must define this method\
             to return a tuple of all serializable objects or None.'''
         return None
-    
+
     def __setstate__(self,state):
         '''When restoring the serialized object from document we have the chance to set some internals here.\
             Since no data were serialized nothing needs to be done here.'''
@@ -161,23 +156,23 @@ class ViewProviderPlane:
         taskd = PlaneTaskPanel(vobj)
         FreeCADGui.Control.showDialog(taskd)
         return True
-    
+
     def doubleClicked(self,vobj):
         taskd = PlaneTaskPanel(vobj)
         FreeCADGui.Control.showDialog(taskd)
         return(True)
-    
+
 
 class CommandPlane:
     "the WingPanel command definition"
- 
+
     def GetResources(self):
         iconpath = os.path.join(smWB_icons_path,'plane.png')
         return {'Pixmap': iconpath, 'MenuText': QtCore.QT_TRANSLATE_NOOP("Create_a_Plane","Create a Plane")}
-    
+
     def IsActive(self):
         return not FreeCAD.ActiveDocument is None
-    
+
     def Activated(self):
         #a=FreeCAD.ActiveDocument.addObject("Part::FeaturePython","plane")
         a=FreeCAD.ActiveDocument.addObject("App::DocumentObjectGroupPython","plane")
@@ -185,7 +180,7 @@ class CommandPlane:
         Plane(a)
         ViewProviderPlane(a.ViewObject)
         FreeCAD.ActiveDocument.recompute()
-            
+
 
 if FreeCAD.GuiUp:
     #register the FreeCAD command
